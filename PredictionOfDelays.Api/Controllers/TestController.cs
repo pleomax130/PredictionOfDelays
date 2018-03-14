006828e84a9c92246cd3a6658a4f4e7aa5528a45
ppraca226140 <ppraca226140@e-science.pl>
2018-03-14 15:45:26 +0100

    Inital commit

---
+++ PredictionOfDelays.Api/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace PredictionOfDelays.Api.Controllers
+{
+    public class TestController : ApiController
+    {
+        public string[] Get()
+        {
+            return new [] {"hehe"};
+        }
+    }
+}
