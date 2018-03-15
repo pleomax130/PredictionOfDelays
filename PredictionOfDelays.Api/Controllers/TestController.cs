552d72f113330dc93969b757c3d93315aeaeb15e
ppraca226140 <ppraca226140@e-science.pl>
2018-03-15 17:43:37 +0100

    Create initial api project

--- PredictionOfDelays.Api/Controllers/TestController.cs
+++ PredictionOfDelays.Api/Controllers/TestController.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http;
+using PredictionOfDelays.Core.Models;
 
 namespace PredictionOfDelays.Api.Controllers
 {
     public class TestController : ApiController
     {
-        public string[] Get()
+        public string Get()
         {
-            return new [] {"hehe"};
+            ApplicationDbContext t = new ApplicationDbContext();
+            t.Users.ToList();
+            return "halo";
         }
     }
 }
--- PredictionOfDelays.Api/Properties/AssemblyInfo.cs
+++ PredictionOfDelays.Api/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PredictionOfDelays.Api")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("74aa0554-793f-47b7-a1d2-802f2af52fbb")]
+[assembly: Guid("e86a6a66-13e5-41b0-8e3a-b8b93a400234")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
