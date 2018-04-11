db9b1387292dffbcb506f0af03bbcd78def45b5c
ppraca226140 <pleomax130@gmail.com>
2018-04-11 20:57:06 +0200

    Enable cors

--- PredictionOfDelays.Api/App_Start/WebApiConfig.cs
+++ PredictionOfDelays.Api/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -13,6 +14,8 @@ namespace PredictionOfDelays.Api
     {
         public static void Register(HttpConfiguration config)
         {
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
